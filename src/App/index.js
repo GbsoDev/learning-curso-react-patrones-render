a32df22773226d16fbc681754003c44a2269d3bd
GbsoDev <gb.so.dev@outlook.com>
2024-08-22 08:37:49 -0500

    Patrones: practicando React.Children.toArray y React.cloneElement para pasar una misma propiedad a múltiples coponentes

--- src/App/index.js
+++ src/App/index.js
@@ -31,16 +31,14 @@ function App() {
   } = useTodo();
   return (
     <>
-      <TodoHeader>
+      <TodoHeader loading={loading}>
         <TodoCounter
           totalTodos={totalTodos}
           completedTodos={completedTodos}
-          loading={loading} 
         />
         <TodoSearch
           searchValue={searchValue}
           setSearchValue={setSearchValue}
-          loading={loading} 
         />
       </TodoHeader>
       <TodoList
--- src/Todo/TodoHeder/index.js
+++ src/Todo/TodoHeder/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-export function TodoHeader({ children }) {
+export function TodoHeader({ children, loading }) {
+    const childrenArray = React.Children.toArray(children);
     return (
         <header>
-            {children}
+            {childrenArray.map(child => React.cloneElement(child, { loading }))}
         </header>
     );
 }
