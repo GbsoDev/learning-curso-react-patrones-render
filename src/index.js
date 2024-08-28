fd916440554ce3db644a16dd4bb403d982815e3d
GbsoDev <gb.so.dev@outlook.com>
2024-08-28 13:26:38 -0500

    Revert Patrones: High Order Components, practica
    This reverts commit 7e79824ac9f4f1ed8d77be8634a936691d9fb864.

--- src/index.js
+++ src/index.js
@@ -1,31 +1,7 @@
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import App from './App';
-
-
-
-
-function App({ saludo, nombre }) {
-  return <h1>!{saludo}, {nombre}¡</h1>
-}
-
-function withSaludo(WrappedComponent) {
-  return function WrappedComponentWithSaludo(saludo) {
-    return function ComponenteSaludo(props) {
-      return (
-        <>
-          <WrappedComponent {...props} saludo={saludo} />
-          <p>Este parrafo de acompañamiento</p>
-        </>
-      );
-    }
-  }
-}
-
-
-const AppWIthSaludo = withSaludo(App)('hola')
-
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<AppWIthSaludo nombre={'Gerson'} />);
+root.render(<App />);
