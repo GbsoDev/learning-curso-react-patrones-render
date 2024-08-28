7e79824ac9f4f1ed8d77be8634a936691d9fb864
GbsoDev <gb.so.dev@outlook.com>
2024-08-28 13:24:41 -0500

    Patrones: High Order Components, practica

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+// import App from './App';
+
+
+
+
+function App({ saludo, nombre }) {
+  return <h1>!{saludo}, {nombre}¡</h1>
+}
+
+function withSaludo(WrappedComponent) {
+  return function WrappedComponentWithSaludo(saludo) {
+    return function ComponenteSaludo(props) {
+      return (
+        <>
+          <WrappedComponent {...props} saludo={saludo} />
+          <p>Este parrafo de acompañamiento</p>
+        </>
+      );
+    }
+  }
+}
+
+
+const AppWIthSaludo = withSaludo(App)('hola')
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<AppWIthSaludo nombre={'Gerson'} />);
